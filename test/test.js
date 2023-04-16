2bbe7900891696ebf1c873cd588d0791c7ff80ee
JT Ziolo <ziolojt@gmail.com>
2023-04-16 10:26:42 -0600

    test(getInvalidIdxsBySquaresRule): pending tests

--- test/test.js
+++ test/test.js
@@ -10,7 +10,8 @@ function trimSudoku (sudoku) {
   return temp
 }
 
-let solvedSudoku = trimSudoku(`
+// Note: invalid
+let filledSudoku = trimSudoku(`
     123 456 789
     234 567 891
     345 678 912
@@ -24,26 +25,86 @@ let solvedSudoku = trimSudoku(`
     912 345 678
 `)
 
-let unsolvedSudoku = trimSudoku(`
-    .23 456 789
-    234 .67 891
-    345 678 912
+// Note: variation of above, still invalid
+let unfilledSudoku = trimSudoku(`
+  .23 456 789
+  234 .67 891
+  345 678 912
 
-    456 789 123
-    567 .91 2.4
-    678 912 345
+  456 789 123
+  567 .91 2.4
+  678 912 345
 
-    789 123 456
-    8.1 234 567
-    912 345 6.8
+  789 123 456
+  8.1 234 567
+  912 345 6.8
 `)
-console.log(solvedSudoku)
+
+let invalidSudoku = trimSudoku(`
+  1.. ..3 2..
+  ... 41. .4.
+  ..1 ..3 ...
+
+  ... .1. ..2
+  313 1.. ...
+  ... .5. .2.
+  
+  2.. ..2 .4.
+  ... 5.. 2..
+  .5. .5. ...
+`)
+
+let solvedSudoku = trimSudoku(`
+  725 139 684
+  198 456 237
+  463 827 591
+
+  572 398 416
+  689 541 723
+  341 672 859
+
+  237 984 165
+  914 265 378
+  856 713 942
+`)
+
+// Variant of solvedSudoku, not guaranteed to have one unique solution, but is
+// guaranteed to be valid
+let validSudoku = trimSudoku(`
+  7.5 13. 684
+  .98 ..6 ..7
+  4.. 8.7 5..
+
+  .7. 3.8 4.6
+  6.. ..1 .23
+  3.1 6.2 ..9
+
+  ..7 ... 16.
+  ..4 ... 3..
+  8.6 ... 942
+`)
+
+describe('invalid indices for each rule', () => {
+  describe('getInvalidIdxsBySquaresRule', () => {
+    it('includes all invalid indices of invalidSudoku', () => {
+      // setup
+      const input = invalidSudoku
+      const expected = []
+      // exercise
+      const result = getInvalidIdxsBySquaresRule(input)
+      // verify
+      assert.strictEqual(result, expected)
+    })
+    it('does not include valid indices of invalidSudoku')
+    it('does not find invalid indices in validSudoku')
+  })
+})
 
 describe('sudoku utility functions', () => {
-  describe('getValueByRowCol', () => {
-    it('returns the correct value for (0, 0) on unsolvedSudoku', () => {
+  describe('getValueByRowCol and getIndexByRowCol', () => {
+    it('returns the correct value for (0, 0) on unfilledSudoku', () => {
       // setup
-      const input = unsolvedSudoku
+      const input = unfilledSudoku
       const expected = '.'
       // exercise
       const result = getValueByRowCol(input, 0, 0)
@@ -51,9 +112,9 @@ describe('sudoku utility functions', () => {
       // verify
       assert.equal(result, expected)
     })
-    it('returns the correct value for (0, 0) on solvedSudoku', () => {
+    it('returns the correct value for (0, 0) on filledSudoku', () => {
       // setup
-      const input = solvedSudoku
+      const input = filledSudoku
       const expected = 1
       // exercise
       const result = getValueByRowCol(input, 0, 0)
@@ -61,9 +122,9 @@ describe('sudoku utility functions', () => {
       // verify
       assert.equal(result, expected)
     })
-    it('returns the correct value for (8, 0) on unsolvedSudoku', () => {
+    it('returns the correct value for (8, 0) on unfilledSudoku', () => {
       // setup
-      const input = unsolvedSudoku
+      const input = unfilledSudoku
       const expected = 9
       // exercise
       const result = getValueByRowCol(input, 8, 0)
@@ -71,9 +132,9 @@ describe('sudoku utility functions', () => {
       // verify
       assert.equal(result, expected)
     })
-    it('returns the correct value for (4, 3) on unsolvedSudoku', () => {
+    it('returns the correct value for (4, 3) on unfilledSudoku', () => {
       // setup
-      const input = unsolvedSudoku
+      const input = unfilledSudoku
       const expected = '.'
       // exercise
       const result = getValueByRowCol(input, 4, 3)
@@ -81,9 +142,9 @@ describe('sudoku utility functions', () => {
       // verify
       assert.equal(result, expected)
     })
-    it('returns the correct value for (4, 3) on solvedSudoku', () => {
+    it('returns the correct value for (4, 3) on filledSudoku', () => {
       // setup
-      const input = solvedSudoku
+      const input = filledSudoku
       const expected = 8
       // exercise
       const result = getValueByRowCol(input, 4, 3)
@@ -93,7 +154,7 @@ describe('sudoku utility functions', () => {
     })
     it('throws RangeError for (-1, *)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       for (let i = 0; i < 9; i++) {
         assert.throws(() => getValueByRowCol(input, -1, i), RangeError)
@@ -101,7 +162,7 @@ describe('sudoku utility functions', () => {
     })
     it('throws RangeError for (*, -1)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       for (let i = 0; i < 9; i++) {
         assert.throws(() => getValueByRowCol(input, i, -1), RangeError)
@@ -109,13 +170,13 @@ describe('sudoku utility functions', () => {
     })
     it('throws RangeError for (-1, -1)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       assert.throws(() => getValueByRowCol(input, -1, -1), RangeError)
     })
     it('throws RangeError for (9, *)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       for (let i = 0; i < 9; i++) {
         assert.throws(() => getValueByRowCol(input, 9, i), RangeError)
@@ -123,7 +184,7 @@ describe('sudoku utility functions', () => {
     })
     it('throws RangeError for (*, 9)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       for (let i = 0; i < 9; i++) {
         assert.throws(() => getValueByRowCol(input, i, 9), RangeError)
@@ -131,7 +192,7 @@ describe('sudoku utility functions', () => {
     })
     it('throws RangeError for (9, 9)', () => {
       // setup
-      const input = unsolvedSudoku // does not matter which
+      const input = unfilledSudoku // does not matter which
       // exercise and verify
       assert.throws(() => getValueByRowCol(input, 9, 9), RangeError)
     })
