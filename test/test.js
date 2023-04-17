b70688e00af08c8f645ccb4227cfae5b63122c75
JT Ziolo <ziolojt@gmail.com>
2023-04-16 20:49:32 -0600

    test: add green tests for isSuperSet test func

--- test/test.js
+++ test/test.js
@@ -96,6 +96,20 @@ function isSuperSet (set, subset) {
   return true
 }
 
+describe('test functions', () => {
+  describe('isSuperSet', () => {
+    it('returns true for [1, 2] as subset of [1. 2. 3. 4. 5]', () => {
+      assert.isTrue(isSuperSet(new Set([1, 2, 3, 4, 5]), new Set([1, 2])))
+    })
+    it('returns false for [1, 2] as superset of [1. 2. 3. 4. 5]', () => {
+      assert.isFalse(isSuperSet(new Set([1, 2]), new Set([1, 2, 3, 4, 5])))
+    })
+    it('returns true for [] as subset of [1. 2. 3. 4. 5]', () => {
+      assert.isTrue(isSuperSet(new Set([1, 2, 3, 4, 5]), new Set()))
+    })
+  })
+})
+
 describe('invalid indices for each rule', () => {
   describe('getInvalidIdxsBySquaresRule', () => {
     it('includes all invalid indices of invalidSudoku', () => {
