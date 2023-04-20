cbf51315629c7213910a6efa0d0df801b12f417d
JT Ziolo <ziolojt@gmail.com>
2023-04-19 21:48:41 -0600

    test(setValueByRowCol): add pending tests

--- test/test-util.js
+++ test/test-util.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert
 const {
   getValueByRowCol,
   getIdxByRowCol,
-  getRowColByIdx,
+  getRowColByIdx
 } = require('../src/index.js')
 const {
   filledSudoku,
@@ -13,6 +13,19 @@ const {
 } = require('./test-constants.js')
 
 describe('sudoku utility functions', () => {
+  describe('setValueByRowCol', () => {
+    it('successfully sets the value at (0, 0) of a blank sudoku')
+    it('successfully sets the value at (3, 4) of a blank sudoku')
+    it('successfully sets the value at (8, 8) of a blank sudoku')
+    it('successfully sets the value at (0, 0) of a filled sudoku')
+    it('successfully sets the value at (3, 4) of a filled sudoku')
+    it('successfully sets the value at (8, 8) of a filled sudoku')
+    it('successfully sets the value at (0, 0) of a partially filled sudoku')
+    it('results in no change when setting a value to the existing value (for a blank sudoku)')
+    it('results in no change when setting a value to the existing value (for a filled sudoku)')
+    it('results in no change when setting a value to the existing value (for a partially filled sudoku)')
+    it('throws an error when attempting to set the value to an illegal value')
+  })
   describe('getRowColByIdx', () => {
     it('is the inverse of getIdxByRowCol, returns original input', () => {
       for (let row = 0; row < 9; row++) {
