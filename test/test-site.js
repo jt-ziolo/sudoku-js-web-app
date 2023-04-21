80d045e2af920b18803d3ddc726bce6d297bbc93
JT Ziolo <ziolojt@gmail.com>
2023-04-21 15:48:43 -0600

    test(initializeSudokuGrid): write failing/red tests

--- test/test-site.js
+++ test/test-site.js
@@ -1,10 +1,46 @@
-import { asset } from 'chai'
-import { initializeSudokuGrid } from '../src/index'
+import { assert } from 'chai'
+import sinon from 'sinon'
+
+import { initializeSudokuGrid } from '../src/index.js'
 
 describe('construct sudoku grid', () => {
   describe('initializeSudokuGrid', () => {
-    it('throws an error when the document object cannot be located')
-    it('successfully adds sudoku grid square elements to the div')
-    it('adds unfilled sudoku grid square elements')
+    describe('defined document object', () => {
+      let document = {
+        idPassedIn: null,
+        childrenPassedIn: [],
+        getElementById (id) {
+          this.idPassedIn = id
+          return {
+            appendChild (child) {
+              this.childrenPassedIn.push(child)
+            }
+          }
+        }
+      }
+      beforeEach(() => {
+        document.idPassedIn = null
+        document.childPassedIn = null
+      })
+      it("successfully calls getElementById with parameter 'sudoku'", () => {
+        // exercise
+        initializeSudokuGrid()
+        // verify
+        assert.equal(document.idPassedIn, 'sudoku')
+      })
+      it('successfully calls appendChild 81 times', () => {
+        // exercise
+        initializeSudokuGrid()
+        // verify
+        assert.lengthOf(document.childrenPassedIn, 81)
+      })
+    })
+    it('throws an error when the document object cannot be located', () => {
+      // setup - none needed, mocha not aware of document object
+      // exercise & verify
+      assert.throws(() => {
+        initializeSudokuGrid()
+      }, ReferenceError)
+    })
   })
 })
